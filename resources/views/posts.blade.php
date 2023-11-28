b4508cbdeb95c9f2a32ef5e43de6dcd4e9c2ccd0
Otabek Mavlonov <mavlonovo@tamu.edu>
2023-11-28 11:52:52 -0600

    made blog post dynamic

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,34 +4,10 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec mi leo. Aliquam ullamcorper condimentum
-            purus ac sagittis. In hac habitasse platea dictumst. Integer sollicitudin felis ut fermentum sodales. Nam
-            venenatis egestas sapien. Vivamus lobortis elit eu dolor dignissim, ac consequat diam lobortis.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec mi leo. Aliquam ullamcorper condimentum
-            purus ac sagittis. In hac habitasse platea dictumst. Integer sollicitudin felis ut fermentum sodales. Nam
-            venenatis egestas sapien. Vivamus lobortis elit eu dolor dignissim, ac consequat diam lobortis.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec mi leo. Aliquam ullamcorper condimentum
-            purus ac sagittis. In hac habitasse platea dictumst. Integer sollicitudin felis ut fermentum sodales. Nam
-            venenatis egestas sapien. Vivamus lobortis elit eu dolor dignissim, ac consequat diam lobortis.
-        </p>
-    </article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post;?>
+        </article>
+    <?php endforeach; ?>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+
+
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    
+
     return view('post', [
         'post' => Post::find($slug)
     ]);
