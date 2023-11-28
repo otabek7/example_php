655c97e28ca078b8422727162fb981367a7ff975
Mavlonov <otabekm@dsa.reldom.tamu.edu>
2023-11-28 08:52:54 -0600

    updated routes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,13 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post"></a>My First Post</h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec mi leo. Aliquam ullamcorper condimentum
-            purus ac sagittis. In hac habitasse platea dictumst. Integer sollicitudin felis ut fermentum sodales. Nam
-            venenatis egestas sapien. Vivamus lobortis elit eu dolor dignissim, ac consequat diam lobortis.
-        </p>
+        <?= $post;?>
     </article>
 
     <a href="/">Go Back</a>
