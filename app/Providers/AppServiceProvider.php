cb50042eebe53d1c5c88459a2f4c7c1f7de1f41d
Otabek Mavlonov <mavlonovo@tamu.edu>
2023-12-01 15:53:38 -0600

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
+
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
